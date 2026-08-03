842222bd45baf3dca0492c542057f3cfd32262eb
caio <caiov1nn1@hotmail.com>
2026-08-02 21:03:17 -0300

    feat: implement initial CLI parser with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod commands;
+mod config;
+mod docker;
+mod error;
+mod git;
+mod ui;
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Status => {
+            println!("Executando: crew status...");
+            // Aqui você chamará a função do src/commands/status.rs no futuro
+        }
+        Commands::Up => {
+            println!("Executando: crew up...");
+        }
+    }
 }
